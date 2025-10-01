4f7b324341fea950744ae739147586ac9992fdec
1butenko <ihor.butenko@icloud.com>
2025-10-01 21:19:42 +0300

    fix | fixed motion elements

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,6 @@
-import { StaticImageData } from "next/image";
-
 export interface StructureColsProps {
     header : string;
     paragph1 : string;
     paragph2 : string;
     span : string;
-    image: StaticImageData;
 }
