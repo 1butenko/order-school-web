5bcc5f49be2e5bd55d472af20425bef685ebb8e0
1butenko <ihor.butenko@icloud.com>
2025-10-10 23:20:31 +0300

    dev | edit nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,6 +3,7 @@
 import React from "react"
 import Image from "next/image"
 import logo from "@/assets/logo.png"
+import logo_dark from "@/assets/logo_dark.png"
 
 import {
   NavigationMenu,
@@ -26,10 +27,10 @@ export function Navigation() {
   return (
     <header
       className={`w-full h-36 flex items-center justify-between px-24 py-16 font-mono fixed top-0 left-0 z-50 transition-all duration-300
-        ${scrolled ? "bg-background text-black shadow-md border-border rounded-br-lg rounded-bl-xl" : "bg-transparent text-white"}
+        ${scrolled ? "bg-background text-foreground shadow-md border-border rounded-br-lg rounded-bl-lg" : "bg-transparent text-white"}
       `}
     >
-      <Image src={logo} alt="Logo" height={36} />
+      <Image src={scrolled ? logo_dark : logo} alt="Logo" height={36} />
       <NavigationMenu className="hidden md:block">
         <NavigationMenuList className="font-medium text-lg uppercase">
           <NavigationMenuItem>
